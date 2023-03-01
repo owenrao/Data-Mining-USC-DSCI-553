ab33ea6a2291704c1c0a4dada063818ea3bb9d93
Ruijie Rao <79633483+owenrao@users.noreply.github.com>
2023-03-01 17:28:33 +0800

    HW6

---
+++ Clustering/task.py
@@ -0,0 +1,13 @@
+import numpy as np
+import random
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import manhattan_distances, euclidean_distance
+
+def main(input_path, n_cluster, output_path):
+    with open(input_path) as file:
+        for line 
+
+if __name__ == '__main__':
+    import sys
+
+    main(sys.argv[1],sys.argv[2],sys.argv[3])
